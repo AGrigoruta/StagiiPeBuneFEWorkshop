b138f0acf7155089bf502dd8f73612b2e3509728
AGrigoruta <alexander.grigoruta@gmail.com>
2018-07-31 11:00:24 +0300

    This is the point where stopped on bot groups. Additional styling needed on the messages, and the profile pic should be added as well. Feel free to play around :D

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,9 @@
+
 import { Component, OnInit, Input } from '@angular/core';
 import { ChatService } from '../chat.service';
+// ============================== DOWN HERE ===============================
+import { ApiServiceService } from '../api-service.service';
+// ============================== UP HERE =================================
 
 @Component({
   selector: 'app-chat',
@@ -9,14 +13,41 @@ import { ChatService } from '../chat.service';
 export class ChatComponent implements OnInit {
 
   @Input() chatService: ChatService;
+  messages: any;
   // Declare needed variables to display messages 
 
-  constructor() {
+  constructor(private api: ApiServiceService) {
     // Write function to retrieve messages, both in the database and live ones
-    // Hint: look at app.component
+    // Hint: look at app.component in constructor
+    
   }
 
   ngOnInit() {
+    this.api.getMessages().subscribe((resp) => {
+      this.messages = resp;
+      this.messages.forEach((item) => {
+        if (this.chatService.getUser() && 
+            item.firstName === this.chatService.getUser().firstName &&
+            item.lastName === this.chatService.getUser().lastName) {
+              item.me = true;
+            } else {
+              item.me = false;
+            }
+      })
+      console.log(this.messages);
+    });
+
+    this.chatService.getMessages().subscribe((resp) => {
+      if (this.chatService.getUser() && 
+            resp.firstName === this.chatService.getUser().firstName &&
+            resp.lastName === this.chatService.getUser().lastName) {
+              resp.me = true;
+            } else {
+              resp.me = false;
+            }
+      this.messages.push(resp);
+      console.log(resp);
+    })
   }
 
 }
